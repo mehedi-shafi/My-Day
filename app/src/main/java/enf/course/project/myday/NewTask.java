627f5f1177eeb6578a813d6802dd46ce3014a257
Mehedi Shafi <mehedishafi@hotmail.com>
2017-12-07 18:59:14 +0600

    added new task

---
+++ app/src/main/java/enf/course/project/myday/NewTask.java
@@ -0,0 +1,13 @@
+package enf.course.project.myday;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NewTask extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_task);
+    }
+}
